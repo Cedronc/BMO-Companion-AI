f279376b6c5d1d956d29c2796ca930b11f3c3d7e
Cedronc <cedricmrtl@pm.me>
2025-10-28 00:38:52 +0100

    feat: volume steps and unmute

--- Media.py
+++ Media.py
@@ -1,9 +1,10 @@
 import keyboard
 ##Media keys are used to hopefully cover both windows and linux (even macos?)
-def media_control(media_key: str):
+def media_control(media_key: str, steps: int = 1):
     """
     Create media control functions at runtime,
-    Params: a media key to be played could be any of these:
+    Params:
+    a media key to be played could be any of these:
         'play/pause media',
         'next track', 
         'previous track
@@ -11,6 +12,18 @@ def media_control(media_key: str):
         'volume down',
         'volume mute'
         And must be a string (str)
+    steps optional param for how many volume up's or down's to execute default is 1 (int)
     returns nothing
     """
-    keyboard.send(media_key)
+    print(steps//2)
+    match media_key:
+        case 'volume up' | 'volume down':
+            for i in range((steps//2) - 1):
+                print(i)
+                keyboard.send(media_key)
+        case 'unmute volume' | 'volume unmute':
+            keyboard.send('volume mute')
+        case _:
+            keyboard.send(media_key)
+
+
